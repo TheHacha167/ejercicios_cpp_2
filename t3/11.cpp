100e8124da0a2571a17812ad577211c85071294e
Jesus_hna <thehacha167@gmail.com>
2023-05-22 18:13:55 +0200

    .

--- t3/11.cpp
+++ t3/11.cpp
@@ -1,24 +1,4 @@
 /*
 
-Escriba un programa que solicite una edad (un entero) e indique en la salida est´andar
-si la edad introducida est´a en el rango [18; 25].
 
 */
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    int edad;
-
-    cout << "Introduce una edad: ";
-    cin >> edad;
-
-    if (edad >= 18 && edad <= 25) {
-        cout << "La edad introducida está en el rango [18; 25]." << endl;
-    } else {
-        cout << "La edad introducida no está en el rango [18; 25]." << endl;
-    }
-
-    return 0;
-}
